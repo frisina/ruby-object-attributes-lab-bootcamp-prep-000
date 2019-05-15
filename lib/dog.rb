8d66f521da0d75fb73c569be329f1d38be59b5ef
Marco Frisina <marco@frisina.xyz>
2019-05-15 16:09:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 class Dog
 
-  def initialize(name)
-    @name = name
+  def name=(new_name)
+    @this_dogs_name = name
   end
 
   def name
-    @name
+    @this_dogs_name
   end
-
-  def name=(new_name)
-    @name = new_name
-  end
+  
 end
